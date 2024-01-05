8a9e66fd0bc0006828850c59b0a5b1b2c68ea722
Sneider Estrada <sneid11@gmail.com>
2024-01-04 22:04:44 -0500

    ADD models entry and  author

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Author(model.model):
+
+    name = models.CharField(
+        max lenght=30,
+        help_text="name of the author"
+        Null=False
+        blank=false
+        ) 
+        
+    last_name = models.CharField(
+        max lenght=30,
+        help_text="last name of the author"
+        Null=False
+        blank=false
+        ) 
+
+     nacionality = models.CharField(
+        max lenght=30,
+        ) 
+
+    email=models.EmailField(
+        null=false
+        blank=false
+        unique=True
+    )
+
+    created=models.DateTimeField(auto_now_add=true)
+    update= models.DateTimeField(auto_now=true)
+
+
+class EntryBlog (models.model): 
+    author=models.ForeingKey(author, on_delete=models.CASCADA, related_name="entry blog")
+    conten= models.textfield()
+
+    
